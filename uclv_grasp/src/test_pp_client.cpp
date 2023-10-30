0ae0af2f63116c7b2eef3250689a8f37fc0c5c01
Marco19987 <marco1998.mds@gmail.com>
2023-10-30 09:31:35 +0100

    Add execution test

--- uclv_grasp/src/test_pp_client.cpp
+++ uclv_grasp/src/test_pp_client.cpp
@@ -2,12 +2,16 @@
 #include "uclv_grasp_interfaces/srv/pick_and_place_traj_srv.hpp"
 #include "uclv_utilities/color.h"
 #include "uclv_utilities/utilities.hpp"
+#include "uclv_moveit_planner_interface/action/traj_action.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
 
 #include <chrono>
 #include <cstdlib>
 #include <memory>
 
 using namespace std::chrono_literals;
+using TrajAction_ = uclv_moveit_planner_interface::action::TrajAction;
+using GoalHandleTrajAction = rclcpp_action::ClientGoalHandle<TrajAction_>;
 
 class DemoNode : public rclcpp::Node
 {
@@ -15,8 +19,12 @@ public:
     bool read_pose = false;
     DemoNode() : Node("demo_node")
     {
+
         // Create the service client to call the planner service
         this->planner_client_ = create_client<uclv_grasp_interfaces::srv::PickAndPlaceTrajSrv>("pp_traj_service", rmw_qos_profile_services_default);
+
+        // Create the action client to call the trajectory execution action
+        this->traj_action_client_ = rclcpp_action::create_client<TrajAction_>(this, "trajectory_execution_as");
 
         // Wait for the service to be available
         while (!planner_client_->wait_for_service(1s))
@@ -48,7 +56,7 @@ public:
         this->pose_sub.reset();
     }
 
-    void get_planning()
+    bool get_planning()
     {
         RCLCPP_INFO_STREAM(this->get_logger(), geometry_msgs::msg::to_yaml(this->pick_pose));
         auto obj_pose_base_link = uclv::transform_pose(this->shared_from_this(), this->pick_pose, "base_link");
@@ -101,13 +109,20 @@ public:
             rclcpp::FutureReturnCode::SUCCESS)
         {
             RCLCPP_ERROR(get_logger(), "Failed to call planner service");
-            return;
+            return false;
         }
 
         auto planner_response_ = planner_response.get();
 
         // Get the trajectory from the response
         bool success_ = planner_response_->success;
+
+        traj_vec.clear();
+        traj_vec.push_back(planner_response_->traj_home_pre_grasp);
+        traj_vec.push_back(planner_response_->traj_pre_grasp_pick);
+        traj_vec.push_back(planner_response_->traj_pick_post_grasp);
+        traj_vec.push_back(planner_response_->traj_post_grasp_pre_place);
+        traj_vec.push_back(planner_response_->traj_pre_place_place);
 
         if (success_)
         {
@@ -118,13 +133,88 @@ public:
             std::cout << BOLDRED << "Planning failed!" << RESET << std::endl;
             rclcpp::shutdown();
         }
+
+        return success_;
+    }
+
+    void send_goal()
+    {
+        using namespace std::placeholders;
+
+        std::cout << BOLDWHITE << "Waiting for action server to start." << RESET << std::endl;
+
+        if (!this->traj_action_client_->wait_for_action_server())
+        {
+            RCLCPP_ERROR(this->get_logger(), "Action server not available after waiting");
+            rclcpp::shutdown();
+        }
+
+        auto goal_msg = TrajAction_::Goal();
+        goal_msg.simulation = false;
+        goal_msg.traj = traj_vec;
+        goal_msg.topic_robot = "joint_states";
+        goal_msg.scale_factor = 1.0;
+        goal_msg.rate = 50.0;
+        std::cout << BOLDWHITE << "Sending goal to the action server" << RESET << std::endl;
+
+        auto send_goal_options = rclcpp_action::Client<TrajAction_>::SendGoalOptions();
+        send_goal_options.goal_response_callback =
+            std::bind(&DemoNode::goal_response_callback, this, _1);
+        send_goal_options.result_callback =
+            std::bind(&DemoNode::result_callback, this, _1);
+        this->traj_action_client_->async_send_goal(goal_msg, send_goal_options);
+    }
+
+    auto getTimer()
+    {
+        return this->timer_;
     }
 
 private:
+    std::vector<moveit_msgs::msg::RobotTrajectory> traj_vec;
     rclcpp::Client<uclv_grasp_interfaces::srv::PickAndPlaceTrajSrv>::SharedPtr planner_client_;
     rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr pose_sub;
     geometry_msgs::msg::PoseStamped pick_pose;
     std::shared_ptr<uclv_grasp_interfaces::srv::PickAndPlaceTrajSrv_Request> planner_request;
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp_action::Client<TrajAction_>::SharedPtr traj_action_client_;
+
+    void goal_response_callback(const GoalHandleTrajAction::SharedPtr &goal_handle)
+    {
+        if (!goal_handle)
+        {
+            RCLCPP_ERROR(this->get_logger(), "Goal was rejected by server");
+        }
+        else
+        {
+            RCLCPP_INFO(this->get_logger(), "Goal accepted by server, waiting for result");
+        }
+    }
+
+    void result_callback(const GoalHandleTrajAction::WrappedResult &result)
+    {
+        switch (result.code)
+        {
+        case rclcpp_action::ResultCode::SUCCEEDED:
+            break;
+        case rclcpp_action::ResultCode::ABORTED:
+            RCLCPP_ERROR(this->get_logger(), "Goal was aborted");
+            return;
+        case rclcpp_action::ResultCode::CANCELED:
+            RCLCPP_ERROR(this->get_logger(), "Goal was canceled");
+            return;
+        default:
+            RCLCPP_ERROR(this->get_logger(), "Unknown result code");
+            return;
+        }
+
+        if (result.result->success)
+            std::cout << BOLDGREEN << "Trajectory execution success!" << RESET << std::endl;
+        else
+            std::cout << BOLDRED << "Trajectory execution failed!" << RESET << std::endl;
+
+        rclcpp::shutdown();
+    }
 };
 
 int main(int argc, char **argv)
@@ -142,9 +232,17 @@ int main(int argc, char **argv)
 
     demo_node->reset_pub();
 
-    demo_node->get_planning();
+    if (demo_node->get_planning())
+    {
+        demo_node->send_goal();
+        // demo_node->getTimer() = demo_node->create_wall_timer(
+        //     std::chrono::milliseconds(500),
+        //     std::bind(&DemoNode::send_goal, demo_node));
+    };
+    
+    
 
-    rclcpp::shutdown();
+    // rclcpp::shutdown();
 
     return 0;
 }
